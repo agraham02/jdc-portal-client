0890d91aaca5dfd5ce75788dcf5c6255da45d1bb
Ahmad Graham <ahmadgrahamdev@gmail.com>
2025-09-03 12:34:36 -0400

    Simplify pages from old structure

--- src/app/(app)/employees/[id]/page.tsx
+++ src/app/(app)/employees/[id]/page.tsx
@@ -1,7 +1,7 @@
 interface Params {
     id: string;
 }
-export default function Page({ params }: { params: Params }) {
+export default function EmployeeDetailsPage({ params }: { params: Params }) {
     return (
         <main>
             <h1>Employee Details</h1>
